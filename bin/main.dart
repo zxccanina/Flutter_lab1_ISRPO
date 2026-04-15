11c1fe877a0fc9bf6e9c81f69d0811bcb54c91ef
ВашÐcokes238 <_cokes238@gmail.com>
2026-04-15 14:33:17 +0300

    learn dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,33 @@
+const appName = "TodoApp";
 void main() {
-  print("Hello world!");
+  String name = "DiRay";
+  String? name2 = null;
+  var count = 0;
+  var title = "Учить Rust (не надо)";
+  var x = 5;
+  //x = "text";
+  final id = 1;
+  print("$name,$name2,$count,$title,$id,$appName");
+  List<String> tags = ["институт", "дом"];
+  Map<String, dynamic> data = {"key": "value", "num": 13};
+  Set<int> ids = {1, 2, 3};
+  dynamic anything = 42;
+  anything = "now a string";
+  String greet(String name) => "Hello, $name!";
+  void printTodo({required String title, bool done = false}){
+    print("${done ? "+":"-"}$title");
+  }
+  printTodo(title: "Купить молоко");
+  printTodo(title: "Проверить код", done: true);
+  String repeat(String s, [int times = 2])=> s*times;
+
+}
+class Todo{
+  Todo({required this.id, required this.title, this.isDone = false});
+  final int id;
+  String title;
+  bool isDone;
+  Todo.empty() :id=0, title='', isDone = false;
+  @override
+  String toString() => "${isDone ? "+":"-"}[$id] $title";
 }
